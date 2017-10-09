6e5fa4d4c6c8dbe3733c79c49da34e8f117ed8dd
Aralvarellosblas <arturo.alvarellos@gmail.com>
2017-10-09 19:39:00 +0200

    Modificacion del ejercicio

---
+++ src/boletin2_01_1/Boletin2_01_1.java
@@ -0,0 +1,11 @@
+package boletin2_01_1;  //Copia del ejercicio 1 del boletin 2
+
+public class Boletin2_01_1 {
+
+    public static void main(String[] args) {
+        float base = 4f, altura =3f, area;
+      area = base* altura /2;
+        System.out.println("Area = "+ area);
+    }
+    
+}
